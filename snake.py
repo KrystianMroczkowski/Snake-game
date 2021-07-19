3ce876c57169a388378244d5178c01a8626ac92e
KrystianMroczkowski <87522500+KrystianMroczkowski@users.noreply.github.com>
2021-07-20 01:12:32 +0200

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,58 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.tail = self.segments[-1]
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def grow(self):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_x = self.tail.xcor()
+        new_y = self.tail.ycor()
+        new_position = (new_x, new_y)
+        new_segment.goto(new_position)
+        self.tail = new_segment
+        self.segments.append(new_segment)
+
+
+
+
